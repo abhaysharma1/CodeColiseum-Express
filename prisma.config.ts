183dc0b89009d3f9f38b4931d3ba6c576258fa33
Abhay Sharma <abhaysharma.mrt@gmail.com>
2026-04-03 00:19:22 +0530

    added temporary varibale for prisma so build doesn;t fail

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
+
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/postgres'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -7,7 +9,7 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: databaseUrl,
   },
 })
         
